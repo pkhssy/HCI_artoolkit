1073833f70ccd132e1cd7bef07deec07b7333e98
pkhssy <32731032+pkhssy@users.noreply.github.com>
2020-12-16 20:46:32 +0900

    vuforia

---
+++ Assets/Scenes/colorChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class colorChange : MonoBehaviour{
+    Renderer cubeColor;
+
+    void Start(){
+        cubeColor = gameObject.GetComponent<Renderer>();
+    }
+
+    void Update(){ // 마우스 버튼의 입력 감지
+        if (Input.GetMouseButtonDown(0)){ // 버튼 입력이 감지될 경우 
+            if (cubeColor.material.color != Color.blue){ // 파란색이 아니라면 파란색으로 변경
+                cubeColor.material.color = Color.blue;
+            } else{ // 파란색인 경우 빨간색으로 변경
+                cubeColor.material.color = Color.red;
+            }
+        }
+    }
+}
+
+
+
+
+
